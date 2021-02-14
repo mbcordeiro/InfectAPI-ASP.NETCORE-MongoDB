9b078789c5e3d088a7265cfe7ff3ea789e6441c2
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2021-02-14 00:58:50 -0300

    Data collections infected

---
+++ InfectedAPI/Data/Collections/Infected.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfectedAPI.Data.Collections
+{
+    public class Infected
+    {
+        public Infected(DateTime dateBirth, string gender, double latitude, double longitude)
+        {
+            DateBirth = dateBirth;
+            Gender = gender;
+            Location = new GeoJson2DGeographicCoordinates(longitude, latitude); ;
+        }
+
+        public DateTime DateBirth { get; set; }
+        public string Gender { get; set; }
+        public GeoJson2DGeographicCoordinates Location { get; set; }
+    }
+}
