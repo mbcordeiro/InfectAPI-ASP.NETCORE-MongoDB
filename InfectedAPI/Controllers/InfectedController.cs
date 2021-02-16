e26f47a0392b0ff0603a2c5c170a551434034c05
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-02-15 23:40:17 -0300

    MongoDB context, model dto and infected controller

---
+++ InfectedAPI/Controllers/InfectedController.cs
@@ -0,0 +1,44 @@
+﻿using InfectedAPI.Data.Collections;
+using InfectedAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfectedAPI.Controllers
+{
+    [Route("api/[infected]")]
+    [ApiController]
+    public class InfectedController : ControllerBase 
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infected> _infectedsCollection;
+
+        public InfectedController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectedsCollection = _mongoDB.DB.GetCollection<Infected>(typeof(Infected).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SaveInfected([FromBody] InfectedDto dto)
+        {
+            var infected = new Infected(dto.DateBirth, dto.Gender, dto.Latitude, dto.Longitude);
+
+            _infectedsCollection.InsertOne(infected);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult GetInfected()
+        {
+            var infecteds = _infectedsCollection.Find(Builders<Infected>.Filter.Empty).ToList();
+
+            return Ok(infecteds);
+        }
+    }
+}
---
+++ InfectedAPI/Models/InfectedDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfectedAPI.Models
+{
+    public class InfectedDto
+    {
+        public DateTime DateBirth { get; set; }
+        public string Gender { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
